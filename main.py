69586cc44bb0a7c4f9ccfad4b56cdc6978c2a826
Fareez RW <iqmal3@outlook.com>
2023-10-25 08:01:03 +0800

    🐛 fix(main.py): fix input file validation to prevent errors when file does not exist ✨ feat(main.py): add support for command line argument to specify input file 🔀 chore(main.py): refactor code to use intermediate folder variable for consistency and readability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from wand.image import Image
 from wand.color import Color
 import os
+import sys
 
 def get_filename(filepath):
     """
@@ -10,24 +11,30 @@ def get_filename(filepath):
     filename_without_extension = os.path.splitext(filename)[0]
     return filename_without_extension
 
+input_file = sys.argv[1]
 
+if not os.path.exists(input_file):
+    print('File does not exist')
+    exit(1)
+
+intermediate_folder = 'intermediate'
 print('Starting conversion')
 print('Creating intermediate folder')
-if not os.path.exists('intermediate'):
-    os.mkdir('intermediate')
+if not os.path.exists(intermediate_folder):
+    os.mkdir(intermediate_folder)
 
 # First pass: Remove background from pdf and convert to png
 # White background may still exists behind text
 # Ref: https://stackoverflow.com/q/27826854/13617136
-with Image(filename='sample.pdf', resolution=300) as img:
+with Image(filename=input_file, resolution=300) as img:
     img.alpha_channel = True
     print('Saving intermediate image(s)')
-    img.save(filename='intermediate/page.png')
+    img.save(filename=f'{intermediate_folder}/page.png')
 
 # To completely remove white background behind text, run a second pass
+# If you're converting from png, you can skip the first pass
 
 # Load image from first pass
-intermediate_folder = 'intermediate'
 files = os.listdir(intermediate_folder)
 images = [os.path.join(intermediate_folder, file) for file in files]
 
@@ -44,6 +51,3 @@ for image in images:
             twenty_percent = int(65535 * 0.2)  # Note: percent must be calculated from Quantum
             img.transparent_color(white, alpha=0.0, fuzz=twenty_percent)
         img.save(filename=f'output/{get_filename(image)}.png')    
-
-
-
